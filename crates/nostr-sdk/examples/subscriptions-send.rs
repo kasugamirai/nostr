d9e0f43fdaba3bb9fbe43a3c38dbf54f36679123
xy <xyzmhx@gmail.com>
2024-04-14 03:02:43 +0900

    Fix connection issue in subscriptions-send example

--- crates/nostr-sdk/examples/subscriptions-send.rs
+++ crates/nostr-sdk/examples/subscriptions-send.rs
@@ -22,6 +22,8 @@ async fn main() -> Result<()> {
         client.add_relay("wss://nostr.oxtr.dev").await.unwrap();
         //client.add_relay("wss://relay.damus.io").await.unwrap();
 
+        client.connect().await;
+
         let subscription = Filter::new()
             .author(public_key)
             .kind(Kind::Metadata)
@@ -31,22 +33,20 @@ async fn main() -> Result<()> {
         let failed_count = Arc::clone(&publish_failed_count);
 
         let handle = spawn(async move {
-            client.connect().await;
-
             let sub_id = client.subscribe(vec![subscription], None).await;
-            println!("Subscription ID: {:?}, {}", sub_id, i + 1);
+            dbg!("Subscription ID: {:?}, {}", sub_id, i + 1);
 
             match client
                 .publish_text_note("My first text note from Nostr SDK!", [])
                 .await
             {
                 Ok(_) => {
-                    println!("Text note published successfully!");
+                    dbg!("Text note published successfully!");
                     let mut success = success_count.lock().unwrap();
                     *success += 1;
                 }
                 Err(e) => {
-                    eprintln!("Failed to publish text note: {}", e);
+                    dbg!("Failed to publish text note: {}", e);
                     let mut failed = failed_count.lock().unwrap();
                     *failed += 1;
                 }
